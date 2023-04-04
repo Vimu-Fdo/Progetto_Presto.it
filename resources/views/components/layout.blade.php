7d8889924cfe3ed7d9c295b648d90339ae191776
Valerio Del Vecchio <valeriodelvecchio@live.com>
2023-04-04 12:35:07 +0200

    Setup iniziale Laravel completato

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="it">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    {{ $slot }}
+  </body>
+</html>
